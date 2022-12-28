295ae0fd6d85fca4fd7326bd01315534212760d6
Ritwik <ritwikkumarrishu@gmail.com>
2022-12-28 16:09:00 +0530

    V1: Complete

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import Wrapper from "../style/NewsCardStyle";
 import Date from "./Date";
 
--- src/page/FavouritePage.js
+++ src/page/FavouritePage.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import NewsPallate from "../components/NewsPallate";
 import Wrapper from "../style/FavouritePageStyle";
 
